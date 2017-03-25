344b534d0f0d88a557c990ac7093583387d874aa
TristanLobaugh <ttrader_atl@yahoo.com>
2017-03-25 19:06:54 -0400

    cleaned up mail.js file

--- server/mail.js
+++ server/mail.js
@@ -1,39 +1,38 @@
 // load aws sdk
-var aws = require('aws-sdk');
+const aws = require('aws-sdk');
 
 // load aws config
 aws.config.loadFromPath('config.js');
 
 // load AWS SES
-var ses = new aws.SES({ apiVersion: '2010-12-01' });
+const ses = new aws.SES({ apiVersion: '2010-12-01' });
 
 // send to list
-var to = ['email@example.com']
+const to = [aws.email];
 
 // this must relate to a verified SES account
-var from = 'emailc@example.com'
+const from = 'emailc@example.com';
 
 
 // this sends the email
 // @todo - add HTML version
-ses.sendEmail( { 
-   Source: from, 
-   Destination: { ToAddresses: to },
-   Message: {
-       Subject: {
-        Source: {
-          Data: 'A Message To You Rudy'
-        }
-       },
-       Body: {
-           Text: {
-               Data: 'Stop your messing around',
-           }
-        }
-   }
-}
-, function(err, data) {
-    if(err) throw err
-        console.log('Email sent:');
-        console.log(data);
- });
+ses.sendEmail({
+	Source: from,
+	Destination: { ToAddresses: to },
+	Message: {
+		Subject: {
+			Source: {
+				Data: 'A Message To You Rudy'
+			}
+		},
+		Body: {
+			Text: {
+				Data: 'Stop your messing around',
+			}
+		}
+	}
+}, (err, data) => {
+	if (err) throw err;
+	console.log('Email sent:');
+	console.log(data);
+});
